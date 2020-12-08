d2b55e2fd91464f52d507c3e774c5c144fec1426
Liew <bl19622@essex.ac.uk>
2020-12-08 14:36:45 +0000

    nice tables mean sd

--- code/helper_func.R
+++ code/helper_func.R
@@ -5,3 +5,13 @@ make_NA <- function (x) {
   return (x)
 
 }
+
+meanNsd_transform <- function (x) {
+
+  m <- round (x[[1]][1], 2)
+  s <- round (x[[1]][2], 2)
+
+  m_s <- paste0(m, "(", s, ")")
+
+  return (m_s)
+}
